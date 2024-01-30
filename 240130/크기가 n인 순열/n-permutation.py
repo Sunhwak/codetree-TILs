068fecb0ac379c022273b260bc68cc95c56d5c74
Sunhwak <157467969+Sunhwak@users.noreply.github.com>
2024-01-30 17:30:37 +0900

    Title: 크기가 n인 순열, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240130/크기가 n인 순열/n-permutation.py
@@ -0,0 +1,27 @@
+n = 3
+visited = [False] * (n+1)
+answer = []
+
+def print_answer() :
+    for ele in answer :
+        print(ele, end = ' ')
+    print()
+
+def pick(cnt) :
+    if cnt == n+1 :
+        print_answer()
+        return
+    
+    for i in range(1, n+1) :
+        if visited[i] :
+            continue
+        
+        visited[i] = True
+        answer.append(i)
+        
+        pick(cnt+1)
+
+        answer.pop()
+        visited[i] = False
+    
+pick(1)
