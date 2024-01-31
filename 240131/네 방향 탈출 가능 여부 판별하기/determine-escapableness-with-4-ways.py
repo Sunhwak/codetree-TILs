47a711d9ce2ddaf7cf9bdcb6b337c86fae855f46
Sunhwak <157467969+Sunhwak@users.noreply.github.com>
2024-01-31 12:19:07 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 193ms, Memory: 26MB, Status: Passed - CodeTree

--- 240131/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240131/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -1,3 +1,37 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+a = [list(map(int, input().split()))for _ in range(n)]
+
+visited = [[0 for _ in range(m)] for _ in range(n)]
+
+def in_range(x, y) :
+    return 0<=x<n and 0<=y<m
+
+def bfs() :
+    while q :
+        r, c = q.popleft() # 현재 위치가 r행 c열을 탐색 중이다.
+        dxs = [-1, 1, 0, 0]
+        dys = [0, 0, -1, 1]
+
+        for dx, dy in zip(dxs, dys) :
+            next_x, next_y = r+dx, c+dy
+            if in_range(next_x, next_y) and a[next_x][next_y] == 1 and not visited[next_x][next_y] :
+                visited[next_x][next_y] = 1
+                q.append((next_x, next_y))
+
+q = deque()
+visited[0][0] = 1
+q.append((0,0))
+
+bfs()
+print(visited[n-1][m-1])
+
+
+'''
+from collections import deque
+
 n, m = map(int, input().split())
 
 grid = [
@@ -17,6 +51,7 @@ visited = [
 
 order = 1
 
+q = deque()
 
 def in_range(x, y):
     return 0<=x<n and 0<=y<m
@@ -34,9 +69,9 @@ def push(x, y) :
     answer[x][y] = order
     order += 1
     visited[x][y] = True
-    q.append(x, y)
+    q.append((x, y))
 
-def bfs(x, y) :
+def bfs() :
     dxs = [-1, 1, 0, 0]
     dys = [0, 0, -1, 1]
 
@@ -48,5 +83,6 @@ def bfs(x, y) :
         if can_go(next_x, next_y) :
             push(next_x, next_y)
 
-
-    x, y
+push(0, 0)
+bfs()
+'''
