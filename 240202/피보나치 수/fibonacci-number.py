35da21659292da7a87897c5f9598bca09e8ab233
Sunhwak <157467969+Sunhwak@users.noreply.github.com>
2024-02-02 10:31:03 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240202/피보나치 수/fibonacci-number.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+dp = [0] * (n+1)
+
+dp[1] = 1
+dp[2] = 1
+
+for i in range(3, n+1) :
+    dp[i] = dp[i-1] + dp[i-2]
+    
+print(dp[n])
+
+
+'''
+n = 6
+memo = [-1] * (n+1)
+
+def fib(n) :
+    if memo[n] != -1 :
+        return memo[n]
+
+    if n<=2:
+        memo[n] = 1
+    else:
+        memo[n] = fib(n-1) + fib(n-2)
+
+    return memo[n]
+'''
