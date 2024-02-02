cb5c94161937209004f2b4ed025bc63869855087
Sunhwak <157467969+Sunhwak@users.noreply.github.com>
2024-02-02 13:02:43 +0900

    Title: 최대 증가 부분 수열, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240202/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240202/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -2,6 +2,21 @@ import sys
 INT_MIN = -sys.maxsize
 
 n = int(input())
+dp = [1] * (n+1)
+a = [1] + list(map(int, input().split()))
+
+for i in range(1, n+1) :
+    for j in range(i) :
+        if a[j] < a[i] :
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
+
+'''
+import sys
+INT_MIN = -sys.maxsize
+
+n = int(input())
 dp = [0] * (n+1)
 a = [0] + list(map(int, input().split()))
 
@@ -19,3 +34,4 @@ for i in range(1, n+1) :
             dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
+'''
