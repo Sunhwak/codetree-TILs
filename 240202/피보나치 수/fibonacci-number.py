3c5a9b8dedf52ec754b6fff9110b5757272c04cb
Sunhwak <157467969+Sunhwak@users.noreply.github.com>
2024-02-02 10:33:23 +0900

    Title: 피보나치 수, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/피보나치 수/fibonacci-number.py
+++ 240202/피보나치 수/fibonacci-number.py
@@ -2,8 +2,11 @@ n = int(input())
 
 dp = [0] * (n+1)
 
-dp[1] = 1
-dp[2] = 1
+if n >= 2 :
+    dp[1] = 1
+    dp[2] = 1
+else :
+    dp[1] = 1
 
 for i in range(3, n+1) :
     dp[i] = dp[i-1] + dp[i-2]
