f4735b63ed6b1e0040bed31fdfd63d2d4f0a1041
Sunhwak <157467969+Sunhwak@users.noreply.github.com>
2024-02-05 19:05:33 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
+++ 240205/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -2,8 +2,6 @@ n, m = map(int, input().split())
 
 def print_one(n, m) :
     for _ in range(n) :
-        for _ in range(m) :
-            print(1, end = '')
-        print()
+        print('1' * m)
 
 print_one(n, m)
